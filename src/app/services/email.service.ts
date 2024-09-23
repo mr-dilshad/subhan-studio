55025b7cf64b0630b8586355b88cf949a54cc5ec
Mohammad Dilshad <mr.dilshad753@gmail.com>
2024-09-23 15:58:34 +0530

    added extra feautures to website

---
+++ src/app/services/email.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  
+  constructor() {}
+
+  sendEmail(formData: any): Promise<EmailJSResponseStatus> {
+    const templateParams = {
+      from_name: formData.name,
+      from_email: formData.email,
+      from_mobile: formData.mobile,
+      for_service: formData.service,
+      message: formData.message
+    };
+
+    return emailjs.send('service_kw9b0up', 'template_jkkv1rh', templateParams, 'ejKKV42sV-54UkwRw');
+  }
+}
