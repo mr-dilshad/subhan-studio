591e0c6984c9c46248b953321b578ebffadc125c
Mohammad Dilshad <mr.dilshad753@gmail.com>
2024-09-27 11:42:43 +0530

    newsletter added

---
+++ src/app/services/subscriber/subscriber.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubscriberService {
+
+  // Use environment variables
+  apiUrl = environment.apiUrl;
+  azureFunctionKey = environment.azureFunctionKey;
+// Azure Function URL
+  private url = `${this.apiUrl}/subscribersave?code=${this.azureFunctionKey}`;
+  constructor(private http: HttpClient) { }
+
+   // Method to save subscriber email
+   saveSubscriberEmail(email: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    // Construct the body as a JSON object
+    const body = {
+      email: email   // This will send { "email": "abc.com" }
+    };
+
+    // Make the POST request
+    return this.http.post(this.url, body, { headers: headers });
+  }
+}
