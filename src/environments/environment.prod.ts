a68cfc51e5462e64aee65f7c1f2d7771498205c6
Mohammad Dilshad <mr.dilshad753@gmail.com>
2024-10-05 12:59:32 +0530

    env changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: process.env.API_URL || '', // Defaults to empty string if not set
+  azureFunctionKey: process.env.AZURE_FUNCTION_KEY || '' // Defaults to empty string if not set
+};
